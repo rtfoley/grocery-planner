4067d8719c11a808948ec2862075f49b5ca3f162
Ryan Foley <rtfoley@gmail.com>
2025-09-15 15:22:44 -0400

    Fix navigation display on mobile

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -1,31 +1,37 @@
 // src/Components/Navigation.tsx
 'use client'
 
-import { Container, Group, Button, Box, ActionIcon, useMantineColorScheme } from '@mantine/core'
+import { Container, Group, Button, Box, ActionIcon, useMantineColorScheme, Burger, Drawer, Stack } from '@mantine/core'
 import { IconSun, IconMoon } from '@tabler/icons-react'
+import { useDisclosure } from '@mantine/hooks'
 import Link from 'next/link'
 
 export function Navigation() {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme()
+  const [opened, { open, close }] = useDisclosure(false)
+
+  const navItems = [
+    { href: '/', label: 'Home' },
+    { href: '/recipes', label: 'Recipes' },
+    { href: '/items', label: 'Items' },
+    { href: '/store-order', label: 'Store Order' }
+  ]
 
   return (
     <Box style={{ borderBottom: '1px solid var(--mantine-color-gray-3)', marginBottom: '1rem' }}>
       <Container size="lg">
         <Group h={60} justify="space-between">
-          <Group>
-            <Button component={Link} href="/" variant="subtle">
-              Home
-            </Button>
-            <Button component={Link} href="/recipes" variant="subtle">
-              Recipes
-            </Button>
-            <Button component={Link} href="/items" variant="subtle">
-              Items
-            </Button>
-            <Button component={Link} href="/store-order" variant="subtle">
-              Store Order
-            </Button>
+          {/* Desktop navigation */}
+          <Group visibleFrom="sm">
+            {navItems.map((item) => (
+              <Button key={item.href} component={Link} href={item.href} variant="subtle">
+                {item.label}
+              </Button>
+            ))}
           </Group>
+          
+          {/* Mobile burger menu */}
+          <Burger opened={opened} onClick={open} hiddenFrom="sm" size="sm" />
           
           <ActionIcon
             onClick={toggleColorScheme}
@@ -37,6 +43,24 @@ export function Navigation() {
           </ActionIcon>
         </Group>
       </Container>
+
+      {/* Mobile drawer */}
+      <Drawer opened={opened} onClose={close} title="Navigation" hiddenFrom="sm">
+        <Stack>
+          {navItems.map((item) => (
+            <Button 
+              key={item.href} 
+              component={Link} 
+              href={item.href} 
+              variant="subtle" 
+              onClick={close}
+              justify="flex-start"
+            >
+              {item.label}
+            </Button>
+          ))}
+        </Stack>
+      </Drawer>
     </Box>
   )
 }
